b07cd89da60baaa42a80215aa5101ecfba8cadec
leonnicolas <60091705+leonnicolas@users.noreply.github.com>
2023-02-26 20:00:51 +0100

    Add README and go generate example (#1)

--- examples/binary/binary.go
+++ examples/binary/binary.go
@@ -6,6 +6,8 @@ import (
 	"github.com/leonnicolas/genstrument/examples/pkg"
 )
 
+//go:generate go run ../../ --file-path binary.go -p Interface --metric-help "help to the metric" --metric-name metric_name_total -o gen.go
+
 type Interface interface {
 	Simple() error
 	ReturnInt() (int error)
